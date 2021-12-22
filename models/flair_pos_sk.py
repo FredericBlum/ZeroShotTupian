52780c0bff29fcab931a55392f3c5b284d206796
frederic <frederic.blum@hu-berlin.de>
2021-12-22 11:37:32 +0100

    fix model save

--- models/flair_pos_sk.py
+++ models/flair_pos_sk.py
@@ -11,18 +11,20 @@ from flair.datasets import UD_ENGLISH
 from helper_functions import create_sk_text
 
 
-create_sk_text('../data/shipibo.conllu', '../data/shipibo_train.txt', sep = False)
-create_sk_text('../data/shipibo_dev.conllu', '../data/shipibo_dev.txt', sep = False)
-create_sk_text('../data/shipibo_test.conllu', '../data/shipibo_test.txt', sep = False)
+create_sk_text('../data/shipibo/orig/shipibo_train.conllu', '../data/shipibo/flair/shipibo_train.txt', sep = False)
+create_sk_text('../data/shipibo/orig/shipibo_dev.conllu', '../data/shipibo/flair/shipibo_dev.txt', sep = False)
+create_sk_text('../data/shipibo/orig/shipibo_test.conllu', '../data/shipibo/flair/shipibo_test.txt', sep = False)
 
 # init a corpus using column format, data folder and the names of the train, dev and test files
 columns = {0: 'text', 1: 'upos'}
-data_folder = '../data/'
+data_folder = '../data/shipibo/flair'
 
 corpus: Corpus = ColumnCorpus(data_folder, columns,
                               train_file = 'shipibo_train.txt',
                               test_file = 'shipibo_test.txt',
                               dev_file = 'shipibo_dev.txt')
+
+# print(corpus)
 
 # create label dictionary for a Universal Part-of-Speech tagging task
 upos_dictionary = corpus.make_label_dictionary(label_type='upos')
@@ -32,7 +34,7 @@ label_type = 'upos'
 # print(corpus.train[1].to_tagged_string('upos'))
 
 # word embeddings
-""" bert_embedding = TransformerWordEmbeddings('bert-base-multilingual-uncased')
+bert_embedding = TransformerWordEmbeddings('bert-base-multilingual-uncased')
 # alternatives: xlm-roberta-large, xlm-roberta-base
 
 # character embeddings
@@ -47,13 +49,10 @@ tagger = SequenceTagger(hidden_size=256,
                         embeddings=embeddings,
                         tag_dictionary=upos_dictionary,
                         tag_type=label_type,
-                        use_crf=True) """
+                        use_crf=True)
 
-tagger = SequenceTagger.load("pos-multi-fast")
+# tagger = SequenceTagger.load("pos-multi-fast")
 # fertiger tagger, nich so gut, vielleicht zero-shot mal anschauen
-
-
-corpus = UD_ENGLISH().downsample(0.01)
 
 
 # 6. initialize trainer
@@ -62,6 +61,7 @@ trainer = ModelTrainer(tagger, corpus)
 # 7. start training
 trainer.train('resources/taggers/example-upos',
                 #write_weights = True,
+                use_final_model_for_eval = True, # necessary because .pt writing is damaged
                 learning_rate=0.1,
                 mini_batch_size=8,
                 max_epochs=20)
@@ -70,6 +70,7 @@ trainer.train('resources/taggers/example-upos',
 plotter = Plotter()
 plotter.plot_training_curves('loss.tsv')
 #plotter.plot_weights('weights.txt')
+
 
 ##########################
 # To-Do: fine-tune embeddings
