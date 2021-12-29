f50bcf9b060174cba13cdece8cc75e430eb1451e
Tarotis <frederic.blum@hu-berlin.de>
2021-12-29 23:40:46 +0100

    new best run 96,6 pos

--- models/flair_pos_sk.py
+++ models/flair_pos_sk.py
@@ -15,20 +15,20 @@ label_type = 'upos'
 ################################
 ### Embeddings               ###
 ################################
-#word_embedding = TransformerWordEmbeddings('bert-base-multilingual-cased', fine_tune=True, layers='-1')
+word_embedding = TransformerWordEmbeddings('bert-base-multilingual-cased', fine_tune=True, layers='-1')
 
 flair_embedding_forward = FlairEmbeddings('models/resources/embeddings/sk_forward/best-lm.pt')
 flair_embedding_backward = FlairEmbeddings('models/resources/embeddings/sk_backward/best-lm.pt')
 #flair_embedding_forward = FlairEmbeddings('models/resources/embeddings/multi_sk_ft/forward/best-lm.pt')
 #flair_embedding_backward = FlairEmbeddings('models/resources/embeddings/multi_sk_ft/backward/best-lm.pt')
 
-embeddings = StackedEmbeddings(embeddings=[#word_embedding,
+embeddings = StackedEmbeddings(embeddings=[word_embedding,
                                            flair_embedding_forward, flair_embedding_backward])
 
 ################################
 ### Tagger and Trainer       ###
 ################################
-tagger = SequenceTagger(hidden_size=256,
+tagger = SequenceTagger(hidden_size=512,
                         embeddings=embeddings,
                         tag_dictionary=upos_dictionary,
                         tag_type=label_type,
@@ -38,9 +38,16 @@ trainer = ModelTrainer(tagger, corpus)
 
 trainer.train('models/resources/taggers/sk_pos',
                 train_with_dev=True,
-                learning_rate=0.2,
+                monitor_train=True,
+                monitor_test=True,
+                anneal_with_restarts=True,
+                use_tensorboard=True,
+                tensorboard_log_dir='models/resources/taggers/sk_pos/tensorboard',
+                metrics_for_tensorboard=[("macro avg", "precision"), ("macro avg", "f1-score")],
+                patience=3,
+                learning_rate=0.01,
                 mini_batch_size=16,
-                max_epochs=40)
+                max_epochs=150)
 
 ###############################
 ### Visualizations          ###
