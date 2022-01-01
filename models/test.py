66c495db8d6f7f99c6715891530d97bb0ead5c94
frederic <frederic.blum@hu-berlin.de>
2022-01-01 16:10:33 +0100

    stupid things

--- models/test.py
+++ models/test.py
@@ -1,3 +1,9 @@
 from helper_functions import write_tupi
+import transformers
 
-write_tupi(write_corpus=True)
+#write_tupi(write_corpus=True)
+
+tokenizer = transformers.BertTokenizer.from_pretrained(
+    "bert-base-multilingual-cased", do_lower_case=False)
+
+print(tokenizer.tokenize("kɨgomət"))
