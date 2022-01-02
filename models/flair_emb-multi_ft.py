4898d2efe4091e595c955b8b4d1c514289467d59
frederic <frederic.blum@hu-berlin.de>
2022-01-02 14:11:09 +0100

    train multi embeddings individually

--- models/flair_emb-multi_ft.py
+++ models/flair_emb-multi_ft.py
@@ -14,22 +14,27 @@ lm_backward = FlairEmbeddings('multi-backward').lm
 
 dictionary: Dictionary = lm_forward.dictionary
 
-corpus_for = TextCorpus("data/combi_emb/3", dictionary, is_forward_lm, character_level = True)
-corpus_back = TextCorpus("data/combi_emb/3", dictionary, is_backward_lm, character_level = True)
+tupinamba = TextCorpus("data/Tupinamba/embeddings", char_dict, is_forward_lm, character_level = True)
+guajajara = TextCorpus("data/Guajajara/embeddings", char_dict, is_forward_lm, character_level = True)
+karo = TextCorpus("data/Karo/embeddings", char_dict, is_forward_lm, character_level = True)
+
+tupinamba_back = TextCorpus("data/Tupinamba/embeddings", char_dict, is_backward_lm, character_level = True)
+guajajara_back = TextCorpus("data/Guajajara/embeddings", char_dict, is_backward_lm, character_level = True)
+karo_back = TextCorpus("data/Karo/embeddings", char_dict, is_backward_lm, character_level = True)
 
 ################################
 ### Trainers                 ###
 ################################
-trainer_forward = LanguageModelTrainer(lm_forward, corpus_for)
-trainer_backward = LanguageModelTrainer(lm_backward, corpus_back)
+trainer_forward = LanguageModelTrainer(lm_forward, tupinamba)
+trainer_backward = LanguageModelTrainer(lm_backward, tupinamba_back)
 
-trainer_forward.train(f'models/resources/embeddings/tupi_3_for_ft',
+trainer_forward.train(f'models/resources/embeddings/tupi_individual/tupinamba_for_ft',
                 sequence_length=80,
                 learning_rate=20,
                 mini_batch_size=32,
                 max_epochs=40)
 
-trainer_backward.train(f'models/resources/embeddings/tupi_3_back_ft',
+trainer_backward.train(f'models/resources/embeddings/tupi_individual/tupinamba_back_ft',
                 sequence_length=80,
                 learning_rate=20,
                 mini_batch_size=32,
--- models/test.py
+++ models/test.py
@@ -1,4 +1,24 @@
-from helper_functions import write_tupi, make_word_dictionary
+from flair.data import MultiCorpus, Sentence
+from flair.models import SequenceTagger
+from helper_functions import make_testset, conllu_to_flair
+from flair.trainers import ModelTrainer
 
 
-write_tupi(write_testset=True)
+################################
+### data and dictionaries    ###
+################################
+akuntsu = make_testset(language = 'Akuntsu')
+kaapor = make_testset(language = 'Kaapor')
+makurap = make_testset(language = 'Makurap')
+munduruku = make_testset(language = 'Munduruku')
+guajajara = make_testset(language = 'Guajajara')
+tupinamba = make_testset(language = 'Tupinamba')
+karo = make_testset(language = 'Karo')
+
+print(akuntsu)
+print(kaapor)
+print(makurap)
+print(munduruku)
+print(guajajara)
+print(tupinamba)
+print(karo)
