687047cb5c3b55cc877b08901338f3c464dfd257
frederic <frederic.blum@hu-berlin.de>
2022-01-03 18:53:06 +0100

    finalize 25-25-50 split

--- models/test.py
+++ models/test.py
@@ -1,6 +1,6 @@
 from flair.data import MultiCorpus, Sentence
 from flair.models import SequenceTagger
-from helper_functions import make_testset, write_tupi, conllu_to_flair
+from helper_functions import make_testset, write_tupi, make_finetuneset
 from flair.trainers import ModelTrainer
 
 
@@ -8,13 +8,13 @@ from flair.trainers import ModelTrainer
 ### data and dictionaries    ###
 ################################
 write_tupi(write_testset=True)
-akuntsu = make_testset(language = 'Akuntsu')
-kaapor = make_testset(language = 'Kaapor')
-makurap = make_testset(language = 'Makurap')
-munduruku = make_testset(language = 'Munduruku')
-guajajara = make_testset(language = 'Guajajara')
-tupinamba = make_testset(language = 'Tupinamba')
-karo = make_testset(language = 'Karo')
+akuntsu = make_finetuneset(language = 'Akuntsu')
+kaapor = make_finetuneset(language = 'Kaapor')
+makurap = make_finetuneset(language = 'Makurap')
+munduruku = make_finetuneset(language = 'Munduruku')
+guajajara = make_finetuneset(language = 'Guajajara')
+tupinamba = make_finetuneset(language = 'Tupinamba')
+karo = make_finetuneset(language = 'Karo')
 
 print(akuntsu)
 print(kaapor)
